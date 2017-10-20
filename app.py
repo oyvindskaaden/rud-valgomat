048312ba155dc39482df0a794eac35cbc6ba6ffa
Torje Eikenes <torje@huddly.com>
2017-10-20 09:42:16 +0200

    displays questions on website

--- app.py
+++ app.py
@@ -3,29 +3,35 @@
 
 from flask import Flask, render_template, request, make_response, redirect, url_for
 import ast
+import json
 
-def store_answer(answer):
+def store_answer(answer, spmFile):
     cookie = ast.literal_eval(request.cookies.get("ans"))
     cookie.append(answer)
+    print(cookie)
 
-    spm = get_question(cookie)
+    spm = get_question(cookie, spmFile)
 
     resp = make_response(render_template("quiz.html", spm=spm))
     resp.set_cookie("ans", str(cookie))
     return resp
 
-def get_question(answers):
+def get_question(answers, spm):
     #returnerer streng med spørsmål
-    return "streng"
+    sprsm = spm[str(len(answers))]["spm"]
+    return sprsm
 
-def answers_done():
+def answers_done(spm):
     #returnerer bool om lenden på cookien er like lang som antall spørsmål
     return False
 
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if answers_done():
+    with open("spm.json", "r") as f:
+        spmFile = json.load(f)
+    if answers_done(spmFile):
         # reroute til /resultat
         return redirect(url_for('resultat'))
 
@@ -33,16 +39,16 @@ def index():
         print(request.form["submit"])
         if request.form["submit"] == "Ja":
             answer = 1
-            resp = store_answer(answer)
+            resp = store_answer(answer, spmFile)
             return resp
         elif request.form["submit"] == "Nei":
             answer = 0
-            rest = store_answer(answer)
+            resp = store_answer(answer, spmFile)
             return resp
     elif request.method == "GET":
         cookie = []
 
-        spm = get_question(cookie)
+        spm = get_question(cookie,spmFile)
 
         resp = make_response(render_template("quiz.html", spm=spm))
         resp.set_cookie("ans", str(cookie))
